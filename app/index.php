d7e628aeacaf1afe51d2ebcd51ff7da2daa106c3
devwl <devwl@LAPTOP-DV.localdomain>
2022-11-02 06:40:49 +0100

    +php multi version

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<?php
+echo "<a href='http://localhost:81'>php8.1</a> | ";
+echo "<a href='http://localhost:80'>php8.0</a> | ";
+echo "<a href='http://localhost:74'>php7.4</a> | ";
+echo "<a href='http://localhost:73'>php7.3</a> | ";
+echo "<a href='http://localhost:72'>php7.2</a> | ";
+echo "<a href='http://localhost:71'>php7.1</a> | ";
+echo "<a href='http://localhost:70'>php7.0</a> | ";
+echo "<a href='http://localhost:56'>php5.6</a><br><br>";
+
+echo "This is index page PHP: " . PHP_VERSION . PHP_EOL . "<br>";
+
+phpinfo();
